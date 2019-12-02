e8325541b1b851bddd84815491280b95687da0e1
Alejandro <Ale@iMac-de-Liliana.local>
2019-12-02 17:11:08 -0500

    mostrando info desde posts

--- posts/views.py
+++ posts/views.py
@@ -6,12 +6,20 @@ posts = [
     {
         'name': 'Alejandr',
         'user': 'a-rodriguez296',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036 '
     }
 ]
 
 
 # Create your views here.
 def list_posts(request):
-    posts = [1,2,3,4]
-    return HttpResponse(str(posts))
+    content = []
+    for post in posts:
+        content.append("""
+            <p>{name}</p>
+            <p>{user}</p>
+            <p>{timestamp}</p>
+            <figure><img src={picture}></figure>
+        """.format(**post))
+    return HttpResponse(content)
