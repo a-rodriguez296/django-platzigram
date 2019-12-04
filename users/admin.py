effc3ca031acdd4dc27dd934c8da84a4863695be
Alejandro <alejo80805@hotmail.com>
2019-12-04 10:48:09 -0500

    editando el admin de perfil

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 from users.models import Profile
 
@@ -28,3 +30,53 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__is_staff',
         'created',
         'modified')
+
+    fieldsets = (
+        ('Profile', {
+            'fields' : ('user', 'picture')
+        }),
+        ('Extra info' , {
+            'fields' : (
+                ('website', 'phone_number'),
+                ('biography')
+            )
+        }), 
+        ('Metadata', {
+            'fields' : (
+                #Como estas dos variables no se pueden editar, hay que crear un readonly
+                ('created', 'modified'),
+            )
+        })
+    )
+
+    readonly_fields = ('created', 'modified', 'user')
+
+    #Esto es para que se pueda crear el profile y el user en la misma pantalla.
+    #Si no hago esto, me toca crear el user por un lado y después crear el profile
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+
+class UserAdmin(BaseUserAdmin):
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+
+#Lo que hay que hacer es hacer unregister del User
+admin.site.unregister(User)
+
+#Luego registrarlo pero como userAdmin
+admin.site.register(User, UserAdmin)
+
+    
