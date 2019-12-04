51f73b091cc74b0f8b8c591d71c3610e614cd10c
Alejandro <Ale@iMac-de-Liliana.local>
2019-12-04 10:22:31 -0500

    editando el admin de perfil

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,28 @@ from django.contrib import admin
 from users.models import Profile
 
 # Register your models here.
-admin.site.register(Profile)
+
+#Esto se hace para customizar el admin. Las clases deben terminar en Admin
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    #Columnas que se muestran en el administrador
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+
+    #Links que me van a llevar al detalle de usuario
+    list_display_links = ('pk', 'user')
+
+    list_editable = ('phone_number', 'website', 'picture')
+
+    search_fields = (
+        'user__username',
+        'user__email', 
+        'user__first_name', 
+        'user__last_name', 
+        'phone_number')
+
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'created',
+        'modified')
