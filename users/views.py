cdbd52c9dce765625f0d0a095390a4d35adb9dda
Alejandro <alejo80805@hotmail.com>
2019-12-08 23:43:41 -0500

    ya se muestra el formulario

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        print('*' * 10)
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, ':', password)
+        print('*' * 10)
+    return render(request, 'users/login.html')
