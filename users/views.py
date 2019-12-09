e9c07e7a846c0b5fdabd33dea4626d67519e4b3d
Alejandro <alejo80805@hotmail.com>
2019-12-09 00:16:04 -0500

    ya se hace el commit

--- users/views.py
+++ users/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_view(request):
     if request.method == 'POST':
-        print('*' * 10)
         username = request.POST['username']
         password = request.POST['password']
-        print(username, ':', password)
-        print('*' * 10)
+        
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            # Esta linea crea la sesión
+            login(request, user)
+            return redirect('feed')
+        else:
+            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
+
     return render(request, 'users/login.html')
