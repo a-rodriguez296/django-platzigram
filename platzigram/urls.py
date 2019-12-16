3bbb3ca6cb0e036690e1462474a91a2f2a84d132
Alejandro <alejo80805@hotmail.com>
2019-12-16 12:58:14 -0500

    logout y sign in

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -35,5 +35,7 @@ urlpatterns = [
     path('posts/', posts_views.list_posts, name='feed'),
 
     path('users/login/', user_views.login_view, name='login'),
+    path('users/logout/', user_views.logout_view, name='logout'),
+    path('users/signup/', user_views.signup_view, name='signup'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+#Exception
+from django.db.utils import IntegrityError
 
 # Create your views here.
 
@@ -18,3 +26,42 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
     return render(request, 'users/login.html')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        
+        username = request.POST['username']
+        password = request.POST['passwd']
+        password_confirmation = request.POST['passwd_confirmation']
+
+        if password != password_confirmation:
+            return redirect('users/signup.html', {'error': 'Passwords do not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already taken'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        #import pdb; pdb.set_trace()
+        if user:
+            # Esta linea crea la sesión
+            login(request, user)
+            return redirect('feed')
+
+
+
+    return render(request, 'users/signup.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
