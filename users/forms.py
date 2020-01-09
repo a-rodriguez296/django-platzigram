cedd93a2a47af72032735ff05cc9d5dc4894bc94
Alejandro <alejo80805@hotmail.com>
2020-01-09 16:21:43 -0500

    utilizando forms para validar el signup de un usuario

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,56 @@
 from django import forms
+from django.contrib.auth.models import User
+from users.models import Profile
+
+
+class SignupForm(forms.Form):
+
+    username = forms.CharField(max_length=50, min_length=4)
+    password = forms.CharField(max_length=70, widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(max_length=70, widget=forms.PasswordInput())
+
+    first_name = forms.CharField(max_length=50, min_length=2)
+    last_name = forms.CharField(max_length=50, min_length=2)
+
+    email = forms.CharField(max_length=50, min_length=8, widget=forms.EmailInput())
+
+
+    #How to validate forms
+    
+    #with this method we can validate a field. The name of the method must be clean_<field's name>
+    def clean_username(self):
+
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in use')
+        #siempre hay q regresar el campo
+        return username
+
+    
+    #With this method we can validate fields that depend on each other. In this case password and password confirmation
+    def clean(self):
+
+        data = super().clean()
+
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+
+        if password != password_confirmation:
+            raise forms.ValidationError('Passwords do not match')
+        
+        return data
+
+    def save(self):
+
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+
+        user = User.objects.create_user(**data)
+        profile = Profile(user=user)
+        profile.save()
+
+
 
 
 class ProfileForm(forms.Form):
--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from users.models import Profile
 
 #Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 #Exception
 from django.db.utils import IntegrityError
@@ -76,35 +76,20 @@ def login_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
-        
-        username = request.POST['username']
-        password = request.POST['passwd']
-        password_confirmation = request.POST['passwd_confirmation']
-
-        if password != password_confirmation:
-            return redirect('users/signup.html', {'error': 'Passwords do not match'})
-
-        try:
-            user = User.objects.create_user(username=username, password=password)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already taken'})
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        #import pdb; pdb.set_trace()
-        if user:
-            # Esta linea crea la sesión
-            login(request, user)
-            return redirect('feed')
-
-
-
-    return render(request, 'users/signup.html')
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={
+            'form':form
+        }
+    )
 
 @login_required
 def logout_view(request):
